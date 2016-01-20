cfd87e51d09ff39f0bc523451fb113c1111ab9bd
Louis Bonicel <louisbonicel@gmail.com>
2016-01-20 20:07:40 +0100

    Update board.h

--- board.h
+++ board.h
@@ -2,14 +2,6 @@
 #ifndef __BOARD_H
 #define __BOARD_H
 
-volatile struct flag {
-	uint8_t button			: 1;
-	uint8_t consigneUpdate	: 1;
-	uint8_t mainProcess		: 1;
-	uint8_t sendData		: 1;
-	uint16_t				: 12;
-} flag;
-
 void LED_Config ( void );
 void PushButton_Config ( void );
 void ADC_GetPotValue ( uint16_t * value );
